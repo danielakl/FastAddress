a0e17aaba8b536923c75cf699bde889c84ea9df3
Daniel Klock <overklock@proton.me>
2026-06-01 13:15:26 +0200

    Add search address query

---
+++ source/FastAddress.Api/Models/SearchStreetAddressQuery.cs
@@ -0,0 +1,18 @@
+using NetTopologySuite.Geometries;
+
+namespace FastAddress.Api.Models;
+
+/// <summary>
+/// Internal steet address search query.
+/// </summary>
+public sealed record SearchStreetAddressQuery
+{
+    /// <summary>Free-text street address query.</summary>
+    public required string Text { get; init; }
+
+    /// <summary>Maximum number of results to return.</summary>
+    public required int Limit { get; init; }
+
+    /// <summary>Point used to bias ranking toward nearby results.</summary>
+    public required Point? LocationBias { get; init; }
+}
